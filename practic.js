bfe03065dd55d7320f62b03c3c595b8b76c7826f
Powerhf <itsupport3@powerhf>
2025-03-26 11:31:40 +0530

    added data sucessfuly

---
+++ practic.js
@@ -0,0 +1,16 @@
+// output
+let greet = "hello";
+
+function changeGreed(){
+    let greet = "Namaste India";
+    console.log(greet);
+
+    function  greedSecond(){
+        console.log(greet);
+    }
+    greedSecond();
+}
+
+console.log(greet);
+
+greedSecond();
